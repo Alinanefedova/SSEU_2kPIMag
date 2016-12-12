69ae18f181f7471932df2772bbc1811ae3cc66ab
Alinanefedova <linkalinka93@mail.ru>
2016-12-12 23:22:15 +0400

    Сортировка по алфавиту Лаб 4 п 4

---
+++ SortAZ/SortAZ/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortAZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слова, разделенные пробелами:");
+            string words = Console.ReadLine();
+            string[] wordsArr = words.Split(' ');
+            Array.Sort(wordsArr);
+            words = wordsArr[0];
+            for (int i = 1; i < wordsArr.Length; i++)
+                words += " " + wordsArr[i];
+            Console.WriteLine("Сортировка слов по алфавиту:");
+            Console.WriteLine(words);
+            Console.ReadKey();
+        }
+    }
+}
