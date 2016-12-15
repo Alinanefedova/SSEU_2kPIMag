6253774617b2ced6a6780381bea2b8147771ec18
Alinanefedova <linkalinka93@mail.ru>
2016-12-15 23:11:31 +0400

    Лаб 3 Задание 3

---
+++ Lab3Zad3/Lab3Zad3/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
