fd6c0ed1e30ced7c7658ebc0f7ca07563f73497f
Alinanefedova <linkalinka93@mail.ru>
2016-12-15 23:26:52 +0400

    Лаб 2 Задание 1

---
+++ Lab4Zad1/Lab4Zad1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
