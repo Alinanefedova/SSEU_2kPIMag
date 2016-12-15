bfe3f8608f506a006db046bc5a70aa75236c0729
Alinanefedova <linkalinka93@mail.ru>
2016-12-15 23:44:36 +0400

    Лаб 3 Зад 2

---
+++ Lab3Z2/Lab3Z2/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
